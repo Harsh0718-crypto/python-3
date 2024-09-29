ddd4a102231e1c95658d93db5fd7091771989254
Harsh0718-crypto <harshrai0718@gmail.com>
2024-09-29 12:42:55 +0530

    Add files via upload

---
+++ 1234.py
@@ -0,0 +1,25 @@
+Python 3.13.0rc2 (tags/v3.13.0rc2:ec61006, Sep  6 2024, 22:13:49) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:/Users/Dell/Desktop/0718.py ====================
+harsh
+>>> 
+==================== RESTART: C:/Users/Dell/Desktop/0718.py ====================
+harsh
+Enter legnth of the rectangle
+==================== RESTART: C:/Users/Dell/Desktop/0718.py ====================
+harsh
+Enter legnth of the rectangle 4
+Enter breadth of the rectangle2
+>>> 
+==================== RESTART: C:/Users/Dell/Desktop/0718.py ====================
+harsh
+Enter legnth of the rectangle4
+Enter breadth of the rectangle2
+>>> area=legnth*breadth
+>>> 
+==================== RESTART: C:/Users/Dell/Desktop/0718.py ====================
+harsh
+Enter legnth of the rectangle 4
+Enter breadth of the rectangle 2
+8.0
